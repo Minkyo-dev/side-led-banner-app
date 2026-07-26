6d0efd20785149b3270701ffa732c168f39d798b
acrfactory <cwhwang0203@gmail.com>
2026-07-26 11:03:36 -0400

    unitId에 test광고 단위 키가 하드코딩 된 부분 수정

--- components/admob/bannerAd.tsx
+++ components/admob/bannerAd.tsx
@@ -7,17 +7,24 @@ import {
 } from "react-native-google-mobile-ads";
 
 // 실제 광고 단위 ID (개발 단계에서는 테스트 광고 단위 ID로 대체)
-const BANNER_AD_UNIT_ID = Platform.select({
+const PROD_BANNER_AD_UNIT_ID = Platform.select({
   ios: "ca-app-pub-3506417530430977/4875843768",
   android: "ca-app-pub-3506417530430977/9971880471",
+  default: TestIds.BANNER,
 });
+
+const isPlaceholderAdUnitId =
+  !PROD_BANNER_AD_UNIT_ID || PROD_BANNER_AD_UNIT_ID.includes("XXXXXXXXXX");
+
+const BANNER_AD_UNIT_ID =
+  __DEV__ || isPlaceholderAdUnitId ? TestIds.BANNER : PROD_BANNER_AD_UNIT_ID;
 
 export default function BannerAdComponent({ style }: any) {
   // Banner Ad Component
   return (
     <View style={[{ alignItems: "center", minHeight: 50 }, style]}>
       <BannerAd
-        unitId={TestIds.BANNER}
+        unitId={BANNER_AD_UNIT_ID}
         size={BannerAdSize.BANNER}
         requestOptions={{
           requestNonPersonalizedAdsOnly: true,
